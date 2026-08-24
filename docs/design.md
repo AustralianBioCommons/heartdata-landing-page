5837db64ca739128d985350ef3eca61dddb8b59a
delocalizer <conrad.leonard@hotmail.com>
2026-08-24 10:48:13 +1000

    fix: restore partner wall behaviour

--- docs/design.md
+++ docs/design.md
@@ -4,23 +4,24 @@
 ### Data Contributors
 This table is the source of truth for cohort Data Contributor orgs and logos.
 
-| --------------------- | ---------------------------------- |---------------------------------------------------------|
-| AusDiab               | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| Baker Biobank         | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| Caught-CAD            | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| EDCAD-PMS             | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| BIRCH (Indigenous)    | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| PREDICT               | Baker Heart and Diabetes Institute | images/logos/png/baker-heart-and-diabetes-institute.png |
-| FIELD                 | The University of Sydney           | images/logos/png/university-of-sydney.png               |
-| BioHEART-CT           | The University of Sydney           | images/logos/png/university-of-sydney.png               |
-| BioHEART-MI           | The University of Sydney           | images/logos/png/university-of-sydney.png               |
-| LIPID                 | The University of Sydney           | images/logos/png/university-of-sydney.png               |
-| Busselton             | University of Western Australia    | images/logos/png/uwa-on-blue.png                                |
-| ASPREE                | Monash University                  | images/logos/png/monash.png                             |
-| 45 and UP             | The SAX Institute                  | images/logos/png/sax.png                                |
-| MCCS                  | Cancer council Victoria            | images/logos/png/cancer-council-victoria.png            |
-| CDAH                  | University of Tasmania             | images/logos/png/utas.png                               |
-| ADVANCE               | The George Institute               | images/logos/png/george-on-purple.png                             |
-| PROPHECY (Indigenous) | SAHMRI                             | images/logos/png/sahmri.png                             |
-| VISION-CAD            | SAHMRI                             | images/logos/png/sahmri.png                             |
 
+| Cohort                | Org                                | Org logo source                                         | Org link url                         |
+| --------------------- | ---------------------------------- | ------------------------------------------------------- | ------------------------------------ |
+| AusDiab               | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| Baker Biobank         | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| Caught-CAD            | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| EDCAD-PMS             | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| BIRCH (Indigenous)    | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| PREDICT               | Baker Heart and Diabetes Institute | images/logos/png/baker-horizontal.png                   | https://www.baker.edu.au/            |
+| FIELD                 | The University of Sydney           | images/logos/png/university-of-sydney.png               | https://www.sydney.edu.au/           |
+| BioHEART-CT           | The University of Sydney           | images/logos/png/university-of-sydney.png               | https://www.sydney.edu.au/           |
+| BioHEART-MI           | The University of Sydney           | images/logos/png/university-of-sydney.png               | https://www.sydney.edu.au/           |
+| LIPID                 | The University of Sydney           | images/logos/png/university-of-sydney.png               | https://www.sydney.edu.au/           |
+| Busselton             | University of Western Australia    | images/logos/png/uwa-on-blue.png                        | https://www.uwa.edu.au/              |
+| ASPREE                | Monash University                  | images/logos/png/monash.png                             | https://www.monash.edu/              |
+| 45 and UP             | The SAX Institute                  | images/logos/png/sax.png                                | https://www.saxinstitute.org.au/     |
+| MCCS                  | Cancer council Victoria            | images/logos/png/cancer-council-victoria.png            | https://www.cancervic.org.au/        |
+| CDAH                  | University of Tasmania             | images/logos/png/utas.png                               | https://www.utas.edu.au/             |
+| ADVANCE               | The George Institute               | images/logos/png/george-on-purple.png                   | https://www.georgeinstitute.org/     |
+| PROPHECY (Indigenous) | SAHMRI                             | images/logos/png/sahmri.png                             | https://sahmri.org.au/               |
+| VISION-CAD            | SAHMRI                             | images/logos/png/sahmri.png                             | https://sahmri.org.au/               |
--- src/components/sections/Partners.tsx
+++ src/components/sections/Partners.tsx
@@ -3,6 +3,8 @@ import SectionWrapper from "../ui/SectionWrapper";
 interface Logo {
   src: string;
   alt: string;
+  /** Partner organisation website (opens in a new tab) */
+  url: string;
   /** Tailwind height class override for visual balance (defaults to h-[3.15rem]) */
   heightClass?: string;
 }
@@ -11,39 +13,39 @@ const groups: { label: string; logos: Logo[] }[] = [
   {
     label: "Leadership",
     logos: [
-      { src: "images/logos/png/australian-cardiovascular-alliance.png", alt: "Australian Cardiovascular Alliance (ACvA)" },
-      { src: "images/logos/png/australian-biocommons.png", alt: "Australian BioCommons" },
-      { src: "images/logos/png/baker-horizontal.png", alt: "Baker Heart and Diabetes Institute", heightClass: "h-[2.8rem]"},
-      { src: "images/logos/png/bioplatforms-australia.png", alt: "Bioplatforms Australia" },
-      { src: "images/logos/png/cad-frontiers.png", alt: "CAD Frontiers" },
+      { src: "images/logos/png/australian-cardiovascular-alliance.png", alt: "Australian Cardiovascular Alliance (ACvA)", url: "https://ozheart.org" },
+      { src: "images/logos/png/australian-biocommons.png", alt: "Australian BioCommons", url: "https://www.biocommons.org.au" },
+      { src: "images/logos/png/baker-horizontal.png", alt: "Baker Heart and Diabetes Institute", url: "https://baker.edu.au", heightClass: "h-[2.8rem]"},
+      { src: "images/logos/png/bioplatforms-australia.png", alt: "Bioplatforms Australia", url: "https://bioplatforms.com" },
+      { src: "images/logos/png/cad-frontiers.png", alt: "CAD Frontiers", url: "https://cadfrontiers.com.au" },
     ],
   },
   {
     label: "Data Contributors",
     logos: [
-      { src: "images/logos/png/baker-heart-and-diabetes-institute.png", alt: "Baker Heart and Diabetes Institute" },
-      { src: "images/logos/png/university-of-sydney.png", alt: "University of Sydney" },
-      { src: "images/logos/png/uwa-on-blue.png", alt: "University of Western Australia" },
-      { src: "images/logos/png/monash.png", alt: "Monash University" },
-      { src: "images/logos/png/cancer-council-victoria.png", alt: "Cancer Council Victoria" },
-      { src: "images/logos/png/utas.png", alt: "University of Tasmania" },
-      { src: "images/logos/png/sax.png", alt: "The SAX Institute" },
-      { src: "images/logos/png/george-on-purple.png", alt: "The George Institute" },
-      { src: "images/logos/png/sahmri.png", alt: "SAHMRI" },
+      { src: "images/logos/png/baker-horizontal.png", alt: "Baker Heart and Diabetes Institute", url: "https://www.baker.edu.au/", heightClass: "h-[2.8rem]" },
+      { src: "images/logos/png/university-of-sydney.png", alt: "University of Sydney", url: "https://www.sydney.edu.au/" },
+      { src: "images/logos/png/uwa-on-blue.png", alt: "University of Western Australia", url: "https://www.uwa.edu.au/" },
+      { src: "images/logos/png/monash.png", alt: "Monash University", url: "https://www.monash.edu/" },
+      { src: "images/logos/png/cancer-council-victoria.png", alt: "Cancer Council Victoria", url: "https://www.cancervic.org.au/" },
+      { src: "images/logos/png/utas.png", alt: "University of Tasmania", url: "https://www.utas.edu.au/" },
+      { src: "images/logos/png/sax.png", alt: "The SAX Institute", url: "https://www.saxinstitute.org.au/" },
+      { src: "images/logos/png/george-on-purple.png", alt: "The George Institute", url: "https://www.georgeinstitute.org/" },
+      { src: "images/logos/png/sahmri.png", alt: "SAHMRI", url: "https://sahmri.org.au/" },
     ],
   },
   {
     label: "Funding",
     logos: [
-      { src: "images/logos/png/national-research-infrastructure-for-australia-ncris.png", alt: "National Research Infrastructure for Australia (NCRIS)" },
-      { src: "images/logos/png/medical-research-future-fund-mrff.png", alt: "Medical Research Future Fund (MRFF)" },
+      { src: "images/logos/png/national-research-infrastructure-for-australia-ncris.png", alt: "National Research Infrastructure for Australia (NCRIS)", url: "https://www.education.gov.au/ncris" },
+      { src: "images/logos/png/medical-research-future-fund-mrff.png", alt: "Medical Research Future Fund (MRFF)", url: "https://www.health.gov.au/our-work/medical-research-future-fund" },
     ],
   },
   {
-    label: "Affiliated",
+    label: "Key Partners",
     logos: [
-      { src: "images/logos/png/university-of-melbourne.png", alt: "University of Melbourne" },
-      { src: "images/logos/png/university-of-sydney.png", alt: "University of Sydney" },
+      { src: "images/logos/png/university-of-melbourne-horiz.png", alt: "University of Melbourne", url: "https://www.unimelb.edu.au" },
+      { src: "images/logos/png/university-of-sydney.png", alt: "University of Sydney", url: "https://www.sydney.edu.au" },
     ],
   },
 ];
@@ -53,25 +55,42 @@ export default function Partners() {
     <SectionWrapper bg="bg-white" labelledBy="partners-heading" className="!py-8 lg:!py-12">
       <h2
         id="partners-heading"
-        className="font-headline text-2xl sm:text-3xl font-bold text-primary text-center mb-6"
+        className="font-headline text-2xl sm:text-3xl font-bold text-primary text-center mb-4"
       >
         Strategic Leadership &amp; Partners
       </h2>
 
+      <p className="max-w-prose mx-auto text-center text-on-surface-variant text-base leading-relaxed mb-2">
+        The ACDC is delivered by a national partnership of cardiovascular research
+        organisations and data-infrastructure providers, funded by the Australian Government
+        through NCRIS and the Medical Research Future Fund, and supported by partner
+        universities. Together these partners provide the scientific leadership, secure
+        infrastructure and governance that underpin Australia&rsquo;s first global
+        standards-based human data commons.
+      </p>
+
       <div className="divide-y divide-outline-light">
         {groups.map((group) => (
           <div key={group.label} className="py-6 text-center">
-            <span className="block text-[1.05rem] uppercase tracking-widest font-semibold text-on-surface-variant mb-4">
+            <span className="block text-sm uppercase tracking-widest font-semibold text-on-surface-variant mb-4">
               {group.label}
             </span>
             <div className="flex flex-wrap items-center justify-center gap-x-8 gap-y-4">
               {group.logos.map((logo) => (
-                <img
+                <a
                   key={logo.alt}
-                  src={`${import.meta.env.BASE_URL}${logo.src}`}
-                  alt={logo.alt}
-                  className={`${logo.heightClass ?? "h-[3.15rem]"} w-auto object-contain mix-blend-multiply`}
-                />
+                  href={logo.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={logo.alt}
+                  className="inline-flex rounded-sm focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-primary"
+                >
+                  <img
+                    src={`${import.meta.env.BASE_URL}${logo.src}`}
+                    alt={logo.alt}
+                    className={`${logo.heightClass ?? "h-[3.15rem]"} w-auto object-contain mix-blend-multiply opacity-80 hover:opacity-100 transition-opacity duration-200`}
+                  />
+                </a>
               ))}
             </div>
           </div>
